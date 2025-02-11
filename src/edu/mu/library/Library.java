962a2602d171bed8c019a1fff04c63d4c7971812
joeS3 <js260292@my.stchas.edu>
2025-02-11 14:24:38 -0600

    Init the Library Project; added an edu.mu.library package, containing a Book class, Library class, and a LibraryApp class holding the main funfunction.

---
+++ src/edu/mu/library/Library.java
@@ -0,0 +1,5 @@
+package edu.mu.library;
+
+public class Library {
+
+}
---
+++ src/edu/mu/library/LibraryApp.java
@@ -0,0 +1,10 @@
+package edu.mu.library;
+
+public class LibraryApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
