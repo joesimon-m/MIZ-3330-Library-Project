0e2def6c86f442bb6137a2a2e0baa8c5331c25e9
joesimon-m <jasdfw@umsystem.edu>
2025-02-12 09:43:59 -0600

    Added parameterized constructor to the Book class.

--- src/edu/mu/library/Book.java
+++ src/edu/mu/library/Book.java
@@ -16,4 +16,23 @@ public class Book {
 		this.ISBN = "Unknown";
 		this.price = 0.0;
 	}
+	
+	/**
+	 * Parameterized constructor which sets each variable to the given variable
+	 * 
+	 * @param title
+	 * 			Given string title to be assigned into this variable title
+	 * @param author
+	 * 			Given string author to be assigned into this variable author
+	 * @param ISBN
+	 * 			Given string ISBN to be assigned into this variable ISBN
+	 * @param price
+	 * 			Given double price to be assigned into this variable price
+	 */
+	public Book(String title, String author, String ISBN, double price) {
+		this.title = title;
+		this.author = author;
+		this.ISBN = ISBN;
+		this.price = price;
+	}
 }	
